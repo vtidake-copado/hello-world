f4584693fdfe38228932961b56aeea76755e6b7f
Vijay Tidake <vtidake@copado.com>
2023-09-07 11:38:31 +0530

    Added comment

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ class HelloWorld {
   public static void main(String[] args) {
     // This is my first hellow world program.
     System.out.println("Hello World !!!");
-    // It will print Hello World
-    // Hope you like it
+    // It will print "Hello World"
+    // Hope you like it. do you?
   }
 }
