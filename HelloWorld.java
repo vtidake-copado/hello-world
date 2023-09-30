540068fcfb0f91d4ff46096d56551d31e2e11089
vtidake-copado <125448782+vtidake-copado@users.noreply.github.com>
2023-09-30 13:49:54 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
+import java.util.concurrent.TimeUnit;
+
 class HelloWorld {
   public static void main(String[] args) {
     // This is my first hellow world program.
     System.out.println("Hello World !!!");
+    TimeUnit.MINUTES.sleep(5);
     // It will print Hello World
     // Hope you like it
   }
