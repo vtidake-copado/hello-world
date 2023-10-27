a53d9fae0a08aeca8699d49fa70f386ccad26994
Vijay Tidake <vtidake@copado.com>
2023-10-27 17:28:02 +0530

    Add runtime param

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 class HelloWorld {
   public static void main(String[] args) {
     // This is my first hellow world program.
-    System.out.println("Hello World !!!");
+    System.out.println("Hello World !!! welcome " + (args.length == 0 ? "guest" : args[0]));
     // It will print "Hello World"
     // Hope you like it. do you?
   }
