80833a66a5f7bffefa998faf678a7a3935414ad7
vtidake-copado <125448782+vtidake-copado@users.noreply.github.com>
2023-11-27 16:14:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,14 @@
 import java.util.concurrent.TimeUnit;
+import java.io.*;
+import java.util.*;
 
 class HelloWorld {
   public static void main(String[] args) throws InterruptedException {
     // This is my first hellow world program.
-    System.out.println("Hello World !!! welcome " + (args.length == 0 ? "guest" : args[0]));
+    System.out.println("Hello World !!! welcome " + (args.length == 0 ? "guest" : Arrays.toString(args)));
     // It will print "Hello World"
     // Hope you like it. do you?
-    TimeUnit.MINUTES.sleep(5);
+    TimeUnit.MINUTES.sleep(2);
     // It will print Hello World
     // Hope you like it
   }
